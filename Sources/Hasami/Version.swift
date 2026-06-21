abece8ba5f7f6dad37bdd53074a82bc69e43e352
Stuart A. Malone <samalone@llamagraphics.com>
2026-06-21 07:59:13 -0400

    Release v1.0.0

--- Sources/Hasami/Version.swift
+++ Sources/Hasami/Version.swift
@@ -3,4 +3,4 @@
 /// This is the single source of truth for the version of the whole project
 /// (the `Hasami` library and both `sukashi` / `sukashi-plan` executables).
 /// It is bumped automatically by `scripts/release.sh`; do not edit by hand.
-public let hasamiVersion = "0.0.0"
+public let hasamiVersion = "1.0.0"
